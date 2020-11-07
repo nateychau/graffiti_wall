7bb54eb50d25a1db18eebeab222b68bc5b16cd12
Yuehan <yuehan.huang@protonmail.com>
2020-11-06 22:11:39 -0800

    Add canvas boundary check

--- src/util.js
+++ src/util.js
@@ -1,7 +1,16 @@
-export const getPosition = (event, canvas) => {
+export const getPosition = function (event, canvas) {
   const bound = canvas.getBoundingClientRect();
   return {
     x: ((event.clientX - bound.left) / bound.width) * canvas.width,
     y: ((event.clientY - bound.top) / bound.height) * canvas.height,
   };
+};
+
+export const isOutOfBound = function (coord, canvas) {
+  return (
+    coord.x > canvas.width ||
+    coord.x < 0 ||
+    coord.y > canvas.height ||
+    coord.y < 0
+  );
 };
