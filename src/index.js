2f72b66f88bc1bee1c355087083f74bd6d2e7811
Yuehan <yuehan.huang@protonmail.com>
2020-11-06 21:59:18 -0800

    Move start position determination when mouse down

--- src/index.js
+++ src/index.js
@@ -1,39 +1,36 @@
-import * as Util from './util.js';
+import * as Util from "./util.js";
 
 let drawing = false;
-let coord;  
+let coord;
 
-window.addEventListener('DOMContentLoaded', (event) => {
-  
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
+window.addEventListener("DOMContentLoaded", (event) => {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
 
-  canvas.addEventListener('mousedown', (e) => {
-    drawing = true; 
+  canvas.addEventListener("mousedown", (e) => {
+    drawing = true;
     coord = Util.getPosition(e, canvas); //get start point for line
-  })
-  
-  canvas.addEventListener('mousemove', (e) => {
-    if(!drawing) return
-    
+    ctx.moveTo(coord.x, coord.y);
+  });
+
+  canvas.addEventListener("mousemove", (e) => {
+    if (!drawing) return;
+
     // ctx.clearRect(0, 0, canvas.width, canvas.height)
-    
+    debugger;
     ctx.beginPath();
     ctx.lineWidth = 5;
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = 'black';
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "black";
 
-    ctx.moveTo(coord.x, coord.y);
     coord = Util.getPosition(e, canvas);
     ctx.lineTo(coord.x, coord.y);
     ctx.stroke();
-  })
+  });
 
-  canvas.addEventListener('mouseup', (e) => {
-    if(!drawing) return
+  canvas.addEventListener("mouseup", (e) => {
+    if (!drawing) return;
 
     drawing = false;
-
-
-  })
-})
+  });
+});
